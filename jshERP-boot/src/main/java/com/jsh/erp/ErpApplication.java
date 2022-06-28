5df1f85d6d69768238b02a4770695908dd41e495
myy520 <478668865@qq.coml>
2022-06-28 13:10:34 +0800

    修尬死配置

--- jshERP-boot/src/main/java/com/jsh/erp/ErpApplication.java
+++ jshERP-boot/src/main/java/com/jsh/erp/ErpApplication.java
@@ -22,7 +22,7 @@ public class ErpApplication{
         ConfigurableApplicationContext context = SpringApplication.run(ErpApplication.class, args);
         Environment environment = context.getBean(Environment.class);
         System.out.println("启动成功，后端服务API地址：http://" + ComputerInfo.getIpAddr() + ":"
-                + environment.getProperty("server.port") + "/jshERP-boot/doc.html");
+                + environment.getProperty("server.port") + "/doc.html");
         System.out.println("您还需启动前端服务，启动命令：yarn run serve 或 npm run serve，测试用户：jsh，密码：123456");
     }
 }
