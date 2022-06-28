6cdb1e808154b8c7790a4c69159a6da67a3e03f1
myy520 <478668865@qq.coml>
2022-06-28 13:09:12 +0800

    修改配置

--- jshERP-web/vue.config.js
+++ jshERP-web/vue.config.js
@@ -1,58 +1,64 @@
 const path = require('path')
 const CompressionPlugin = require('compression-webpack-plugin')
 
-function resolve (dir) {
-    return path.join(__dirname, dir)
+function resolve(dir) {
+  return path.join(__dirname, dir)
 }
 
 // vue.config.js
 module.exports = {
-    // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
-    productionSourceMap: false,
-    configureWebpack: config => {
+  // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
+  productionSourceMap: false,
+  configureWebpack: config => {
     // 生产环境取消 console.log
-        if (process.env.NODE_ENV === 'production') {
-            config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
+    if (process.env.NODE_ENV === 'production') {
+      config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
+    }
+  },
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@$', resolve('src'))
+      .set('@api', resolve('src/api'))
+      .set('@assets', resolve('src/assets'))
+      .set('@comp', resolve('src/components'))
+      .set('@views', resolve('src/views'))
+    // 生产环境，开启js\css压缩
+    if (process.env.NODE_ENV === 'production') {
+      config.plugin('compressionPlugin').use(new CompressionPlugin({
+        test: /\.(js|css|less)$/, // 匹配文件名
+        threshold: 10240, // 对超过10k的数据压缩
+        deleteOriginalAssets: false // 删除源文件
+      }))
+    }
+  },
+  css: {
+    loaderOptions: {
+      less: {
+        modifyVars: {
+          /* less 变量覆盖，用于自定义 ant design 主题 */
+          'primary-color': '#1890FF',
+          'link-color': '#1890FF',
+          'border-radius-base': '4px'
+        },
+        javascriptEnabled: true
+      }
+    }
+  },
+  devServer: {
+    host: '127.0.0.1', // can be overwritten by process.env.HOST
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://gateway.api.com', // 请求本地 需要jshERP-boot后台项目
+        ws: false,
+        changeOrigin: true,
+        logLevel: 'debug',//开发时使用日志级别
+        //secure: false,  // 如果是https接口，需要配置这个参数
+        pathRewrite: {
+          '^/api': ''
         }
-    },
-    chainWebpack: (config) => {
-        config.resolve.alias
-            .set('@$', resolve('src'))
-            .set('@api', resolve('src/api'))
-            .set('@assets', resolve('src/assets'))
-            .set('@comp', resolve('src/components'))
-            .set('@views', resolve('src/views'))
-        // 生产环境，开启js\css压缩
-        if (process.env.NODE_ENV === 'production') {
-            config.plugin('compressionPlugin').use(new CompressionPlugin({
-                test: /\.(js|css|less)$/, // 匹配文件名
-                threshold: 10240, // 对超过10k的数据压缩
-                deleteOriginalAssets: false // 删除源文件
-            }))
-        }
-    },
-    css: {
-        loaderOptions: {
-            less: {
-                modifyVars: {
-                    /* less 变量覆盖，用于自定义 ant design 主题 */
-                    'primary-color': '#1890FF',
-                    'link-color': '#1890FF',
-                    'border-radius-base': '4px'
-                },
-                javascriptEnabled: true
-            }
-        }
-    },
-    devServer: {
-        port: 3000,
-        proxy: {
-            '/jshERP-boot': {
-                target: 'http://localhost:9999', // 请求本地 需要jshERP-boot后台项目
-                ws: false,
-                changeOrigin: true
-            }
-        }
-    },
-    lintOnSave: undefined
+      }
+    }
+  },
+  lintOnSave: undefined
 }
